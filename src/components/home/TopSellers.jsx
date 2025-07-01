16c5ae13aaccc63574d04998b14473bac7076ac9
adamanuszkiewicz <162536934+adamanuszkiewicz@users.noreply.github.com>
2025-06-30 21:06:58 -0700

    changed name of a variable to make more sense

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+
+  const getTopSellers = async () => {
+    try {
+      const response = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+      );
+      setTopSellers(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching Top Sellers", error);
+    }
+  };
+
+  useEffect(() => {
+    getTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -13,26 +32,41 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12">
+          <div className="col-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {topSellers.length > 0
+                ? topSellers.map((seller, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${seller.authorId || ""}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={seller.authorImage || AuthorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${seller.authorId || ""}`}>
+                          {seller.authorName || "Unknown"}
+                        </Link>
+                        <span>{seller.price || "0"} ETH</span>
+                      </div>
+                    </li>
+                  ))
+                : new Array(12).fill(0).map((_, index) => (
+                    <li key={index} className="skeleton-seller">
+                      <div className="skeleton-seller-avatar"></div>
+                      <div className="skeleton-seller-check">
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="skeleton-seller-info">
+                        <div className="skeleton-seller-name"></div>
+                        <div className="skeleton-seller-eth"></div>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>

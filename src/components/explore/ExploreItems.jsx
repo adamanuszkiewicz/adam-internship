ff0446e43e454fcec7838b11e81ed605ddc5f7a4
adamanuszkiewicz <162536934+adamanuszkiewicz@users.noreply.github.com>
2025-07-01 15:21:00 -0700

    retrieved data from a backend API and made the data dynamic and displayed it in the Explore section

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import CountDown from "../CountDown";
 
 const ExploreItems = () => {
+  const [exploreItems, setExploreItems] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(8);
+
+  const getExploreData = async () => {
+    try {
+      const response = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore`
+      );
+      setExploreItems(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching Explore Items", error);
+    }
+  };
+
+  async function filterItems(filter) {
+    const response = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
+    );
+
+    setExploreItems(response.data);
+  }
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 4);
+  };
+
+  useEffect(() => {
+    getExploreData();
+  }, []);
+
+
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          defaultValue=""
+          onChange={(event) => filterItems(event.target.value)}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {exploreItems.slice(0, visibleCount).map((item, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -23,16 +62,19 @@ const ExploreItems = () => {
           <div className="nft__item">
             <div className="author_list_pp">
               <Link
-                to="/author"
+                to={`/author/${item.authorId || ""}`}
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img
+                  className="lazy"
+                  src={item.authorImage || AuthorImage}
+                  alt=""
+                />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
+            {item.expiryDate && <CountDown expiryDate={item.expiryDate} />}
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
                 <div className="nft__item_buttons">
@@ -51,25 +93,38 @@ const ExploreItems = () => {
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+              <Link to={`/item-details/${item.nftId || ""}`}>
+                <img
+                  src={item.nftImage || nftImage}
+                  className="lazy nft__item_preview"
+                  alt=""
+                />
               </Link>
             </div>
             <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+              <Link to={`/item-details/${item.nftId || ""}`}>
+                <h4>{item.title || "No Title"}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
+              <div className="nft__item_info-text">
+                <div className="nft__item_price">
+                  {item.price || "No Price"} ETH
+                </div>
+                <div className="nft__item_like">
+                  <i className="fa fa-heart"></i>
+                  <span>{item.likes || 0}</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       ))}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+        <Link
+          to=""
+          id="loadmore"
+          className="btn-main lead"
+          onClick={handleLoadMore}
+        >
           Load more
         </Link>
       </div>
